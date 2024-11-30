2846949e968de56944f5c7aa4006a8ff37dd723b
CydoEntis <cydoentis@gmail.com>
2024-11-30 10:28:30 -0500

    Removed old auth logic on parties page.

--- src/routes/_authenticated/parties/index.tsx
+++ src/routes/_authenticated/parties/index.tsx
@@ -1,33 +1,25 @@
-import { createFileRoute, redirect } from '@tanstack/react-router'
-import { QueryParams } from '../../../shared/types/types'
-import partiesService from '../../../features/party/api/services/parties.service'
+import { createFileRoute } from "@tanstack/react-router";
+import { QueryParams } from "../../../shared/types/types";
+import partiesService from "../../../features/party/api/services/parties.service";
 
-export const Route = createFileRoute('/_authenticated/parties/')({
-  beforeLoad: ({ context }) => {
-    const isAuthenticated = context.authState.checkIsAuthenticated()
-    if (!isAuthenticated) {
-      throw redirect({
-        to: '/login',
-      })
-    }
-  },
-  component: PartiesPage,
-  validateSearch: (search: Record<string, unknown>): QueryParams => {
-    return {
-      searchTerm: search.query as string | undefined,
-      sortDirection: search.sortDirection as string | undefined,
-      sortField: search.sortField as string | undefined,
-      dateFilterField: search.dateFilterField as string | undefined,
-      pageNumber: search.pageNumber as number | undefined,
-      startDate: search.startDate as string | undefined,
-      endDate: search.endDate as string | undefined,
-    }
-  },
-  loader: async () => {
-    return await partiesService.getAllParties()
-  },
-})
+export const Route = createFileRoute("/_authenticated/parties/")({
+	component: PartiesPage,
+	validateSearch: (search: Record<string, unknown>): QueryParams => {
+		return {
+			searchTerm: search.query as string | undefined,
+			sortDirection: search.sortDirection as string | undefined,
+			sortField: search.sortField as string | undefined,
+			dateFilterField: search.dateFilterField as string | undefined,
+			pageNumber: search.pageNumber as number | undefined,
+			startDate: search.startDate as string | undefined,
+			endDate: search.endDate as string | undefined,
+		};
+	},
+	loader: async () => {
+		return await partiesService.getAllParties();
+	},
+});
 
 function PartiesPage() {
-  return <div>Hello "/parties/"!</div>
+	return <div>Hello "/parties/"!</div>;
 }
