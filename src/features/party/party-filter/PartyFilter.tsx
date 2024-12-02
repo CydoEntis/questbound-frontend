1551e1e9272b46ce1462c8f77445ed7276488574
Cydo <cydoentis@gmail.com>
2024-12-01 22:25:56 -0500

    updated mantine to 7.14.3

---
+++ src/features/party/party-filter/PartyFilter.tsx
@@ -0,0 +1,164 @@
+import {
+  Button,
+  Checkbox,
+  Divider,
+  Flex,
+  Modal,
+  Stack,
+  Text,
+} from "@mantine/core";
+import { useForm, zodResolver } from "@mantine/form";
+import { useNavigate, useSearch } from "@tanstack/react-router";
+import { z } from "zod";
+import { Route } from "../../../routes/_authenticated/parties/index";
+
+type FilterModalProps = {
+  filterOpened: boolean;
+  handleCloseFilterModal: () => void;
+};
+
+function PartyFilter({
+  filterOpened,
+  handleCloseFilterModal,
+}: FilterModalProps) {
+  const { sortField, dateFilterField } = useSearch({
+    from: "/_authenticated/parties/",
+  });
+
+  const sortOptions = [
+    { label: "Title", value: "title" },
+    { label: "Created At", value: "created-at" },
+    { label: "Updated At", value: "updated-at" },
+  ];
+
+  const dateOptions = [
+    { label: "Created On", value: "created-at" },
+    { label: "Updated On", value: "updated-at" },
+  ];
+
+  const orderOptions = [
+    { label: "Ascending", value: "asc" },
+    { label: "Descending", value: "desc" },
+  ];
+
+  const navigate = useNavigate({ from: Route.fullPath });
+
+  const filterFormSchema = z.object({
+    sortBy: z.string(),
+    filterDate: z.string(),
+  });
+
+  type FilterFormValues = z.infer<typeof filterFormSchema>;
+
+  const form = useForm<FilterFormValues>({
+    validate: zodResolver(filterFormSchema),
+    initialValues: {
+      sortBy: sortField ?? "title",
+      filterDate: dateFilterField ?? "created-at",
+    },
+  });
+
+  const updateFilters = (name: keyof FilterFormValues, value: unknown) => {
+    form.setValues({
+      ...form.values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const values = form.values;
+
+    const result = filterFormSchema.safeParse(values);
+    if (result.success) {
+      navigate({
+        search: (prevSearch) => ({
+          ...prevSearch,
+          sortField: result.data.sortBy,
+          dateFilterField: result.data.filterDate,
+        }),
+      });
+
+      handleCloseFilterModal();
+    } else {
+      console.log("Validation failed", result.error.errors);
+    }
+  };
+
+  const handleClearFilters = () => {
+    form.reset();
+    navigate({
+      search: (prevSearch) => ({
+        ...prevSearch,
+        sortField: undefined,
+        dateFilterField: undefined,
+      }),
+    });
+    handleCloseFilterModal();
+  };
+
+  return (
+    <Modal
+      opened={filterOpened}
+      onClose={handleCloseFilterModal}
+      centered
+      title="Filters"
+      size="lg"
+    >
+      <form onSubmit={handleSubmit}>
+        <Stack gap={8} w="100%">
+          {/* Sort Options */}
+          {sortOptions && (
+            <Stack py={8}>
+              <Text>Sort</Text>
+              <Divider />
+              {sortOptions.map(({ label, value }) => (
+                <Checkbox
+                  key={value}
+                  checked={form.values.sortBy === value}
+                  onChange={() => updateFilters("sortBy", value)}
+                  label={label}
+                />
+              ))}
+            </Stack>
+          )}
+
+          {/* Date Options */}
+          {dateOptions && (
+            <Stack py={8}>
+              <Text>Date</Text>
+              <Divider />
+              {dateOptions.map(({ label, value }) => (
+                <Checkbox
+                  key={value}
+                  checked={form.values.filterDate === value}
+                  onChange={() => updateFilters("filterDate", value)}
+                  label={label}
+                />
+              ))}
+            </Stack>
+          )}
+
+          {/* Action Buttons */}
+          <Flex gap={8}>
+            <Button fullWidth color="violet" variant="light" type="submit">
+              Apply Filters
+            </Button>
+            <Button
+              fullWidth
+              color="red"
+              variant="light"
+              type="button"
+              onClick={handleClearFilters}
+            >
+              Clear Filters
+            </Button>
+          </Flex>
+        </Stack>
+      </form>
+    </Modal>
+  );
+}
+
+export default PartyFilter;
--- src/pages/parties/PartiesPage.tsx
+++ src/pages/parties/PartiesPage.tsx
@@ -6,8 +6,8 @@ import { useEffect } from "react";
 import { ActionIcon, Box, Flex, Group } from "@mantine/core";
 import PageHeader from "../../components/page/PageHeader";
 import { Settings2 } from "lucide-react";
-import FilterModal from "../../components/filter/FIlterModal";
 import { useDisclosure } from "@mantine/hooks";
+import PartyFilter from "../../features/party/party-filter/PartyFilter";
 
 type Props = {};
 
@@ -16,23 +16,6 @@ function PartiesPage({}: Props) {
   const { setParties } = usePartyStore();
   const queryParams = searchParams;
   const { data: parties, isLoading, isError } = useGetParties(queryParams);
-
-
-  const sortOptions = [
-    { label: "Title", value: "title" },
-    { label: "Created At", value: "created-at" },
-    { label: "Updated At", value: "updated-at" },
-  ];
-
-  const dateOptions = [
-    { label: "Created On", value: "created-at" },
-    { label: "Updated On", value: "updated-at" },
-  ];
-
-  const orderOptions = [
-    { label: "Ascending", value: "asc" },
-    { label: "Descending", value: "desc" },
-  ];
 
   const [isFilterOpened, { open: openFilters, close: closeFilters }] =
     useDisclosure(false);
@@ -44,7 +27,11 @@ function PartiesPage({}: Props) {
   }, [parties, setParties]);
 
   if (isLoading) {
-    return <p>Loading parties...</p>;
+    return (
+      <>
+        <p>Loading parties...</p>
+      </>
+    );
   }
 
   if (isError) {
@@ -55,39 +42,23 @@ function PartiesPage({}: Props) {
     return <p>No parties found.</p>;
   }
 
-
-
   return (
     <>
-      <FilterModal
+      <PartyFilter
         filterOpened={isFilterOpened}
-        sortOptions={sortOptions}
-        dateOptions={dateOptions}
-        orderOptions={orderOptions}
         handleCloseFilterModal={closeFilters}
-        // onApplyFilters={handleApplyFilters}
-        // onClearFilters={handleClearFilters}
       />
       <PageHeader title="Joined Parties">
         <Flex align="end" justify="space-between">
-          {/* <SearchBar
-            form={form}
-            onSearch={handleSearchSubmit}
-            onClear={resetSearchAndFetch}
-            resetCallback={(reset) => {
-              callbacksRef.current.search = reset;
-            }}
-          /> */}
           <Group align="end">
             <ActionIcon
               size="lg"
               variant="light"
               color="violet"
-                onClick={openFilters}
+              onClick={openFilters}
             >
               <Settings2 size={20} />
             </ActionIcon>
-            {/* <LayoutOptions /> */}
           </Group>
         </Flex>
       </PageHeader>
