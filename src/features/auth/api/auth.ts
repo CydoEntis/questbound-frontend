8607b474f0b6801e36075391a2cda33d33526fd7
Cydo <cydoentis@gmail.com>
2024-12-04 06:25:55 -0500

    Updated auth.ts and apiClient to use new useAuthStore methods for managing state, loginUser, logoutUser, refreshTokens

--- src/features/auth/api/auth.ts
+++ src/features/auth/api/auth.ts
@@ -2,237 +2,218 @@ import { useMutation } from "@tanstack/react-query";
 import authServices from "./services/auth.service";
 import useAuthStore from "../../../stores/useAuthStore";
 import {
-	LoginResponse,
-	LoginRequest,
-	TokensResponse,
-	RegisterRequest,
-	ForgotPasswordRequest,
-	ChangePasswordRequest,
-	ResetPasswordRequest,
+  LoginResponse,
+  LoginRequest,
+  TokensResponse,
+  RegisterRequest,
+  ForgotPasswordRequest,
+  ChangePasswordRequest,
+  ResetPasswordRequest,
 } from "../shared/types";
 import localStorageService from "./services/localStorage.service";
 import { notifications } from "@mantine/notifications";
 
 export function useLogin() {
-	const { setUser, setTokens } = useAuthStore();
+  const { loginUser } = useAuthStore();
 
-	return useMutation({
-		mutationFn: async (
-			credentials: LoginRequest,
-		): Promise<LoginResponse> => {
-			return await authServices.loginUser(credentials);
-		},
-		onSuccess: (data) => {
-			console.log(data);
+  return useMutation({
+    mutationFn: async (credentials: LoginRequest): Promise<LoginResponse> => {
+      return await authServices.loginUser(credentials);
+    },
+    onSuccess: (data) => {
+      loginUser(data);
 
-			setUser(data.user);
-			setTokens(data.tokens);
+      localStorageService.setItem("collabParty", data);
 
-			const collabParty = {
-				user: data.user,
-				tokens: data.tokens,
-			};
-
-			localStorageService.setItem("collabParty", collabParty);
-
-			notifications.show({
-				title: "Success",
-				message: "Login successful!",
-				color: "green",
-				position: "top-right",
-			});
-		},
-		onError: (error: Error) => {
-			notifications.show({
-				title: "Login Failed",
-				message: "Something went wrong!",
-				color: "red",
-				position: "top-right",
-			});
-			throw error;
-		},
-	});
+      notifications.show({
+        title: "Success",
+        message: "Login successful!",
+        color: "green",
+        position: "top-right",
+      });
+    },
+    onError: (error: Error) => {
+      notifications.show({
+        title: "Login Failed",
+        message: "Something went wrong!",
+        color: "red",
+        position: "top-right",
+      });
+      throw error;
+    },
+  });
 }
 
 export function useRegister() {
-	const { setUser, setTokens } = useAuthStore();
+  const { loginUser } = useAuthStore();
 
-	return useMutation({
-		mutationFn: async (
-			credentials: RegisterRequest,
-		): Promise<LoginResponse> => {
-			return await authServices.registerUser(credentials);
-		},
-		onSuccess: (data) => {
-			setUser(data.user);
-			setTokens(data.tokens);
+  return useMutation({
+    mutationFn: async (
+      credentials: RegisterRequest
+    ): Promise<LoginResponse> => {
+      return await authServices.registerUser(credentials);
+    },
+    onSuccess: (data) => {
+      loginUser(data);
 
-			const collabParty = {
-				user: data.user,
-				tokens: data.tokens,
-			};
+      localStorageService.setItem("collabParty", data);
 
-			localStorageService.setItem("collabParty", collabParty);
-
-			notifications.show({
-				title: "Success",
-				message: "Login successful!",
-				color: "green",
-				position: "top-right",
-			});
-		},
-		onError: (error: Error) => {
-			notifications.show({
-				title: "Login Failed",
-				message: "Something went wrong!",
-				color: "red",
-				position: "top-right",
-			});
-			throw error;
-		},
-	});
+      notifications.show({
+        title: "Success",
+        message: "Login successful!",
+        color: "green",
+        position: "top-right",
+      });
+    },
+    onError: (error: Error) => {
+      notifications.show({
+        title: "Login Failed",
+        message: "Something went wrong!",
+        color: "red",
+        position: "top-right",
+      });
+      throw error;
+    },
+  });
 }
 
 export function useRefreshTokens() {
-	const { setTokens } = useAuthStore();
+  const { refreshTokens } = useAuthStore();
 
-	return useMutation({
-		mutationFn: async (tokens: TokensResponse): Promise<TokensResponse> => {
-			return await authServices.refreshTokens(tokens);
-		},
-		onSuccess: (data) => {
-			console.log(data);
-			setTokens(data);
+  return useMutation({
+    mutationFn: async (tokens: TokensResponse): Promise<TokensResponse> => {
+      return await authServices.refreshTokens(tokens);
+    },
+    onSuccess: (data) => {
+      refreshTokens(data);
 
-			const collabParty = { token: data.accessToken };
-			const existingData = localStorageService.getItem("collabParty") || {};
-			const updatedData = { ...existingData, ...collabParty };
+      const collabParty = { token: data.accessToken };
+      const existingData = localStorageService.getItem("collabParty") || {};
+      const updatedData = { ...existingData, ...collabParty };
 
-			localStorageService.setItem("collabParty", updatedData);
+      localStorageService.setItem("collabParty", updatedData);
 
-			return data;
-		},
-		onError: (error: Error) => {
-			notifications.show({
-				title: "Login Expired",
-				message: "You are no longer logged in.",
-				color: "red",
-				position: "top-right",
-			});
-			throw error;
-		},
-	});
+      return data;
+    },
+    onError: (error: Error) => {
+      notifications.show({
+        title: "Login Expired",
+        message: "You are no longer logged in.",
+        color: "red",
+        position: "top-right",
+      });
+      throw error;
+    },
+  });
 }
 
 export function useLogout() {
-	const { setUser, setTokens } = useAuthStore();
+  const { logoutUser } = useAuthStore();
 
-	return useMutation({
-		mutationFn: async (tokens: TokensResponse): Promise<void> => {
-			await authServices.logoutUser(tokens);
-		},
-		onSuccess: () => {
-			setUser(null);
-			setTokens(null);
+  return useMutation({
+    mutationFn: async (tokens: TokensResponse): Promise<void> => {
+      await authServices.logoutUser(tokens);
+    },
+    onSuccess: () => {
+      logoutUser();
+      localStorageService.removeItem("collabParty");
 
-			localStorageService.removeItem("collabParty");
-
-			notifications.show({
-				title: "Success",
-				message: "Logout successful!",
-				color: "green",
-				position: "top-right",
-			});
-		},
-		onError: (error: Error) => {
-			notifications.show({
-				title: "Login Failed",
-				message: "Something went wrong!",
-				color: "red",
-				position: "top-right",
-			});
-			throw error;
-		},
-	});
+      notifications.show({
+        title: "Success",
+        message: "Logout successful!",
+        color: "green",
+        position: "top-right",
+      });
+    },
+    onError: (error: Error) => {
+      notifications.show({
+        title: "Login Failed",
+        message: "Something went wrong!",
+        color: "red",
+        position: "top-right",
+      });
+      throw error;
+    },
+  });
 }
 
 export function useForgotPassword() {
-	return useMutation({
-		mutationFn: async (email: ForgotPasswordRequest): Promise<void> => {
-			await authServices.forgotPassword(email);
-		},
-		onSuccess: (data) => {
-			console.log(data);
+  return useMutation({
+    mutationFn: async (email: ForgotPasswordRequest): Promise<void> => {
+      await authServices.forgotPassword(email);
+    },
+    onSuccess: (data) => {
+      console.log(data);
 
-			notifications.show({
-				title: "Success",
-				message: "Email has been sent.",
-				color: "green",
-				position: "top-right",
-			});
-		},
-		onError: (error: Error) => {
-			notifications.show({
-				title: "Success",
-				message: "Email has been sent.",
-				color: "green",
-				position: "top-right",
-			});
-			throw error;
-		},
-	});
+      notifications.show({
+        title: "Success",
+        message: "Email has been sent.",
+        color: "green",
+        position: "top-right",
+      });
+    },
+    onError: (error: Error) => {
+      notifications.show({
+        title: "Success",
+        message: "Email has been sent.",
+        color: "green",
+        position: "top-right",
+      });
+      throw error;
+    },
+  });
 }
 
 export function useChangePassword() {
-	return useMutation({
-		mutationFn: async (request: ChangePasswordRequest): Promise<void> => {
-			await authServices.changePassword(request);
-		},
-		onSuccess: (data) => {
-			console.log(data);
+  return useMutation({
+    mutationFn: async (request: ChangePasswordRequest): Promise<void> => {
+      await authServices.changePassword(request);
+    },
+    onSuccess: (data) => {
+      console.log(data);
 
-			notifications.show({
-				title: "Success",
-				message: "Email has been sent.",
-				color: "green",
-				position: "top-right",
-			});
-		},
-		onError: (error: Error) => {
-			notifications.show({
-				title: "Success",
-				message: "Email has been sent.",
-				color: "green",
-				position: "top-right",
-			});
-			throw error;
-		},
-	});
+      notifications.show({
+        title: "Success",
+        message: "Email has been sent.",
+        color: "green",
+        position: "top-right",
+      });
+    },
+    onError: (error: Error) => {
+      notifications.show({
+        title: "Success",
+        message: "Email has been sent.",
+        color: "green",
+        position: "top-right",
+      });
+      throw error;
+    },
+  });
 }
 
 export function useResetPassword() {
-	return useMutation({
-		mutationFn: async (request: ResetPasswordRequest): Promise<void> => {
-			await authServices.resetPassword(request);
-		},
-		onSuccess: (data) => {
-			console.log(data);
+  return useMutation({
+    mutationFn: async (request: ResetPasswordRequest): Promise<void> => {
+      await authServices.resetPassword(request);
+    },
+    onSuccess: (data) => {
+      console.log(data);
 
-			notifications.show({
-				title: "Success",
-				message: "Password has been reset.",
-				color: "green",
-				position: "top-right",
-			});
-		},
-		onError: (error: Error) => {
-			notifications.show({
-				title: "Error",
-				message: "Unable to reset password.",
-				color: "red",
-				position: "top-right",
-			});
-			throw error;
-		},
-	});
+      notifications.show({
+        title: "Success",
+        message: "Password has been reset.",
+        color: "green",
+        position: "top-right",
+      });
+    },
+    onError: (error: Error) => {
+      notifications.show({
+        title: "Error",
+        message: "Unable to reset password.",
+        color: "red",
+        position: "top-right",
+      });
+      throw error;
+    },
+  });
 }
--- src/lib/api/apiClient.ts
+++ src/lib/api/apiClient.ts
@@ -5,50 +5,50 @@ import authServices from "../../features/auth/api/services/auth.service";
 import LocalStorageService from "../../features/auth/api/services/localStorage.service";
 
 const apiClient = axios.create({
-	baseURL: baseUrl,
+  baseURL: baseUrl,
 });
 
 apiClient.interceptors.request.use(
-	(request) => {
-		const { user, tokens } = useAuthStore.getState();
-		if (user && tokens) {
-			request.headers["Authorization"] = `Bearer ${tokens.accessToken}`;
-		}
-		return request;
-	},
-	(error) => {
-		return Promise.reject(error);
-	},
+  (request) => {
+    const { user, tokens } = useAuthStore.getState();
+    if (user && tokens) {
+      request.headers["Authorization"] = `Bearer ${tokens.accessToken}`;
+    }
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 apiClient.interceptors.response.use(
-	(response) => response,
-	async (error) => {
-		const originalRequest = error.config;
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
 
-		if (error.response?.status === 401 && !originalRequest._retry) {
-			originalRequest._retry = true;
-			const { user, tokens, setTokens, setUser } = useAuthStore.getState();
-			try {
-				if (user && tokens && tokens.refreshToken) {
-					const newTokens = await authServices.refreshTokens(tokens);
-					setTokens(newTokens);
-					originalRequest.headers["Authorization"] =
-						`Bearer ${newTokens.accessToken}`;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const { user, tokens, refreshTokens, logoutUser } =
+        useAuthStore.getState();
+      try {
+        if (user && tokens && tokens.refreshToken) {
+          const newTokens = await authServices.refreshTokens(tokens);
+          refreshTokens(newTokens);
+          originalRequest.headers["Authorization"] =
+            `Bearer ${newTokens.accessToken}`;
 
-					return apiClient(originalRequest);
-				}
-			} catch (refreshError) {
-				console.log("Token refresh failed, logging out.");
-				authServices.logoutUser(tokens!);
-				setTokens(null);
-				setUser(null);
-				LocalStorageService.clearStorage();
-				window.location.href = "/login";
-			}
-		}
-		return Promise.reject(error);
-	},
+          return apiClient(originalRequest);
+        }
+      } catch (refreshError) {
+        console.log("Token refresh failed, logging out.");
+        authServices.logoutUser(tokens!);
+        logoutUser();
+        LocalStorageService.clearStorage();
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default apiClient;
