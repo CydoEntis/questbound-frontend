bd455d5b594f6091d1ede8a7eb9bbc833c1ab37e
Cydo <cydoentis@gmail.com>
2025-01-03 17:39:22 -0500

    Updated layout of comment section

--- src/features/party/components/quest-comps/comments/CommentSection.tsx
+++ src/features/party/components/quest-comps/comments/CommentSection.tsx
@@ -1,7 +1,6 @@
 import {
   Button,
   Group,
-  ScrollArea,
   Stack,
   Textarea,
   Title,
@@ -15,18 +14,21 @@ import { QuestComment } from "../../../shared/quest.types";
 import { useState } from "react";
 import { Trash2 } from "lucide-react";
 import AvatarDisplay from "../../../../avatar/components/avatar-display/AvatarDisplay";
+import useUserStore from "../../../../../stores/useUserStore";
+import useGetColorTheme from "../../../../../components/theme/hooks/useGetColorScheme";
 
 function CommentSection({
   questId,
   comments,
 }: {
   questId: number;
-  partyMemberId: number;
   comments: QuestComment[];
 }) {
+  const { userId } = useUserStore();
   const [newComment, setNewComment] = useState("");
   const addComment = useAddComment();
   const deleteComment = useDeleteComment();
+  const { isLightMode } = useGetColorTheme();
 
   const handleAddComment = async () => {
     if (newComment.trim() === "") return;
@@ -38,45 +40,15 @@ function CommentSection({
     await deleteComment.mutateAsync({ questId, commentId });
   };
 
-  console.log(comments);
+  const lightModeColor = "#F1EDFE";
+  const darkModeColor = "#312B43";
+  const lightModeTextColor = "black";
+  const darkModeTextColor = "white";
 
   return (
     <Stack gap={4}>
       <Title order={5}>Comments</Title>
-      {comments.length > 0 ? (
-        <ScrollArea h={200}>
-          <Stack gap={8}>
-            {comments.map((comment) => (
-              <Group key={comment.id}>
-                <Paper bg="secondary" variant="light" withBorder p={8} w="100%">
-                  <Stack w="100%">
-                    <Flex justify="space-between" align="center" w="100%">
-                      <Group>
-                        <AvatarDisplay avatar={comment.partyMember.avatar} />
-                        <Text>{comment.partyMember.username}</Text>
-                      </Group>
-                      <ActionIcon
-                        variant="light"
-                        color="red"
-                        onClick={() => handleDeleteComment(comment.id)}
-                      >
-                        <Trash2 size={20} />
-                      </ActionIcon>
-                    </Flex>
-                  </Stack>
-                  <Text size="sm" mt={12}>
-                    {comment.content}
-                  </Text>
-                </Paper>
-              </Group>
-            ))}
-          </Stack>
-        </ScrollArea>
-      ) : (
-        <Text size="sm" c="dimmed">
-          No comments yet. Be the first to add one!
-        </Text>
-      )}
+
       <Textarea
         classNames={{ input: "input" }}
         autosize
@@ -91,6 +63,72 @@ function CommentSection({
           Add Comment
         </Button>
       </Group>
+
+      {comments.length > 0 ? (
+        <Stack gap={8}>
+          {comments.map((comment) => {
+            const isCurrentUser = comment.partyMember.id === userId;
+            return (
+              <Flex
+                key={comment.id}
+                justify={isCurrentUser ? "flex-end" : "flex-start"}
+                w="100%"
+              >
+                <Paper
+                  withBorder
+                  bg={
+                    isCurrentUser
+                      ? isLightMode
+                        ? lightModeColor
+                        : darkModeColor
+                      : "card"
+                  }
+                  p={8}
+                  w="70%"
+                  style={{
+                    alignSelf: isCurrentUser ? "flex-end" : "flex-start",
+                  }}
+                >
+                  <Stack>
+                    <Flex justify="space-between" align="center">
+                      <Group>
+                        <AvatarDisplay avatar={comment.partyMember.avatar} />
+                        <Text>{comment.partyMember.username}</Text>
+                      </Group>
+                      {isCurrentUser && (
+                        <ActionIcon
+                          variant="light"
+                          color="red"
+                          onClick={() => handleDeleteComment(comment.id)}
+                        >
+                          <Trash2 size={20} />
+                        </ActionIcon>
+                      )}
+                    </Flex>
+                    <Text
+                      size="sm"
+                      mt={2}
+                      style={{
+                        color: isCurrentUser
+                          ? isLightMode
+                            ? lightModeTextColor
+                            : darkModeTextColor
+                          : "inherit",
+                      }}
+                    >
+                      {comment.content}
+                    </Text>
+                  </Stack>
+                </Paper>
+              </Flex>
+            );
+          })}
+        </Stack>
+      ) : (
+        <Text size="sm" c="dimmed">
+          No comments yet. Be the first to add one!
+        </Text>
+      )}
     </Stack>
   );
 }
